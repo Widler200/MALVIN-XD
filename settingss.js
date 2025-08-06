a8809f029bf71bd8f45d3010b1365d4fd1aeaa10
Widler200 <hakilajames@gmail.com>
2025-08-06 19:28:57 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,15 @@
 
 module.exports = {
+  SESSION_ID: "
+module.exports = {
   SESSION_ID: "",  // add your Session Id here
+  
+  OWNER_NUMBER: "263714757857", // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+};",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
